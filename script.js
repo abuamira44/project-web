f38d300201a09742e3b43ba8b270063826110f79
abuamira44 <145227273+abuamira44@users.noreply.github.com>
2023-11-14 10:34:18 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// script.js
+
+document.addEventListener("DOMContentLoaded {
+    document.querySelector("form").addEventListener("submit", function(event) {
+      event.preventDefault();
+      
+      var name = document.getElementById("name").value;
+      var email = document.getElementById("email").value;
+      
+      // Perform any necessary form validation or data processing here
+      
+      alert("Thank you, " + name + "! Your message has been });
